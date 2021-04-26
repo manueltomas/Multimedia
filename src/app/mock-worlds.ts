e80bcd750bbdab7b698bdb931b797b183fb6576b
João Afonso <johnlandon1999@gmail.com>
2021-04-26 16:07:11 +0100

    Versao do hub world que permite mudar de ecra

--- src/app/mock-worlds.ts
+++ src/app/mock-worlds.ts
@@ -2,5 +2,6 @@ import { World } from "./World";
 
 export const WORLDS: World[] = [
     {id:1, name:"city"},
-    {id:2, name:"forest"}
+    {id:2, name:"forest"},
+	{id:3, name:"mountain"}
   ];
--- src/app/worlds/worlds.component.ts
+++ src/app/worlds/worlds.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { WORLDS } from '../mock-worlds';
+import { WorldService } from '../world.service';
 
 
 @Component({
@@ -10,13 +11,40 @@ import { WORLDS } from '../mock-worlds';
 })
 export class WorldsComponent implements OnInit {
 
-  constructor(private router : Router) { }
+  constructor(
+	private router : Router,
+	private worldService : WorldService) { }
 
   ngOnInit(): void {
+
   }
   
   chooseWorld(){
 	  this.router.navigate(['animation',WORLDS[0].id])
+  }
+  
+  current = 1;
+  
+  getWorld(){
+    return `assets/img/${this.worldService.getWorldById(this.current).name}HubWorld.png`
+  }
+  
+  getWorldName(){
+	 return this.worldService.getWorldById(this.current).name
+  }
+
+  previousWorld(){
+    this.current = ((this.current-1) % 4)
+    if(this.current == 0){
+      this.current = 3;
+    }
+  }
+
+  nextWorld(){
+    this.current = ((this.current+1) % 4)
+    if(this.current == 0){
+      this.current++;
+    }
   }
 
 }
