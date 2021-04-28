276aee66f308cd60ae0e42038fff757b770830ee
João Afonso <johnlandon1999@gmail.com>
2021-04-28 18:31:18 +0100

    Mock com os animais atualizado de acordo com as imagens

--- src/app/mock-animals.ts
+++ src/app/mock-animals.ts
@@ -3,9 +3,9 @@ import { WORLDS } from "./mock-worlds";
 
 export const ANIMALS: Animal[] = [
     {id:1, name:'cat', description:'', imageUrl:'../assets/img/cat.png', catched:false, world:WORLDS[0].id, page:4},
-    {id:2, name:'dog', description:'', imageUrl:'../assets/img/cat.png', catched:false, world:WORLDS[0].id, page:4},
-    {id:3, name:'rabbit', description:'', imageUrl:'../assets/img/cat.png', catched:false, world:WORLDS[0].id, page:4},
-    {id:4, name:'frog', description:'', imageUrl:'../assets/img/cat.png', catched:false, world:WORLDS[1].id, page:4},
-    {id:5, name:'horse', description:'', imageUrl:'../assets/img/cat.png', catched:false, world:WORLDS[0].id, page:4},
-    {id:6, name:'wolve', description:'', imageUrl:'../assets/img/cat.png', catched:false, world:WORLDS[0].id, page:4},
+    {id:2, name:'dog', description:'', imageUrl:'../assets/img/dog.png', catched:false, world:WORLDS[0].id, page:4},
+    {id:3, name:'rabbit', description:'', imageUrl:'../assets/img/rabbit.png', catched:false, world:WORLDS[1].id, page:4},
+    {id:4, name:'squirrel', description:'', imageUrl:'../assets/img/squirrel.png', catched:false, world:WORLDS[1].id, page:4},
+    {id:5, name:'fox', description:'', imageUrl:'../assets/img/fox.png', catched:false, world:WORLDS[2].id, page:4},
+    {id:6, name:'boar', description:'', imageUrl:'../assets/img/boar.png', catched:false, world:WORLDS[2].id, page:4},
   ];
