8904e35905b0a2c834bcda4915d2596e91424967
manueltomas <manuel.tomas1@outlook.com>
2021-05-01 18:27:29 +0100

    first version of world choice implemented

--- src/app/worlds/worlds.component.ts
+++ src/app/worlds/worlds.component.ts
@@ -12,7 +12,12 @@ import { WorldService } from '../world.service';
 })
 export class WorldsComponent implements OnInit {
   current;
-  
+  source : HTMLSourceElement;
+  video : HTMLVideoElement;
+
+  rodandoEsquerda;
+  rodandoDireita;
+
   constructor(
 	private router : Router,
 	private worldService : WorldService,
@@ -25,35 +30,11 @@ export class WorldsComponent implements OnInit {
 	  }
 	  console.log ("current ", this.current );
 
-    // var videoAux : any = document.getElementById("video")
-    // var video : HTMLVideoElement = videoAux
-    // video.currentTime = 11.7206
-    // var animation = video.animate(
-    //   [
-    //     {currentTime: 11.7206},
-    //     {currentTime: 0}
-    //   ],
-    //   {
-    //     duration: 1
-    //   }
-    // )
-    // animation.currentTime = animation.effect.timing.duration / 2;
-    // animation.playbackRate = 1;
-    // animation.play();
-
-    /*console.log(video)
-    var intervalRewind;
-    video.currentTime = 11.7206;
-    video.playbackRate = 1;
-    intervalRewind = setInterval(function(){
-       if(video.currentTime == 0){
-           clearInterval(intervalRewind);
-           video.pause();
-       }
-       else{
-           video.currentTime += -.01;
-       }
-      },1);*/
+    var source : any = document.getElementById("source")
+    this.source = source
+    var videoAux : any = document.getElementById("video")
+    this.video = videoAux;
+    this.changeVideo()
   }
   
   chooseWorld(){
@@ -70,20 +51,75 @@ export class WorldsComponent implements OnInit {
   }
 
   previousWorld(){
-    this.current = ((this.current-1) % 4)
-    if(this.current == 0){
-      this.current = 3;
-    }
+    this.current = this.previous()
     this.worldService.changeWorld(this.current)
     this.animalService.refresh();
+    this.rodandoEsquerda = true;
+    this.changeVideo()
+  }
+
+  previous(){
+    var result = ((this.current-1) % 4)
+    if(result <= 0){
+      result = 3;
+    }
+    return result;
   }
 
   nextWorld(){
-    this.current = ((this.current+1) % 4)
-    if(this.current == 0){
-      this.current++;
-    }
+    this.current = this.next()
     this.worldService.changeWorld(this.current)
     this.animalService.refresh();
+    this.rodandoDireita = true
+    this.changeVideo()
+  }
+
+  next(){
+    var result = ((this.current+1) % 4)
+    if(result <= 0){
+      result++;
+    }
+    return result;
+  }
+
+  changeVideo(){
+    var sourceAux : any = document.getElementById("source")
+    var source : HTMLSourceElement = sourceAux
+    var videoAux : any = document.getElementById("video")
+    var video : HTMLVideoElement = videoAux;
+    var aux2 = this
+    console.log(this.previous())
+    video.pause();
+    if(this.rodandoDireita){
+      console.log(`assets/video/worlds/worlds${this.previous()}${this.current}.mp4`)
+      source.src = `assets/video/worlds/worlds${this.previous()}${this.current}.mp4`
+      video.onended = function(){
+        source.src = `assets/video/worlds/worlds${aux2.current}.mp4`
+        aux2.rodandoDireita = false
+        console.log(source.src)
+        video.load();
+        video.play()
+      }
+    }else if(this.rodandoEsquerda){
+      source.src = `assets/video/worlds/worlds${this.current}${this.next()}.mp4`
+      video.onended = function(){
+        source.src = `assets/video/worlds/worlds${aux2.current}.mp4`
+        aux2.rodandoEsquerda = false
+        console.log(source.src)
+        video.load();
+        video.play()
+      }
+    }else{
+      source.src = `assets/video/worlds/worlds${this.current}.mp4` 
+    }
+    /*video.onended = function(){
+      source.src = `assets/video/worlds/worlds${aux2.current}.mp4`
+      console.log(source.src)
+      video.load();
+      video.play()
+    }*/
+    video.load();
+    video.play()
+    console.log(source.src)
   }
 }
