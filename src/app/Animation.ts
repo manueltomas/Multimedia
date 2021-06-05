6e7a7c6acc5fb8d7b197a1c49fcb90c324d5c6b0
João Afonso <johnlandon1999@gmail.com>
2021-06-05 15:32:19 +0100

    mock de animation e interface

---
+++ src/app/Animation.ts
@@ -0,0 +1,11 @@
+import { World } from "./World";
+
+export interface Animation{
+    id:number;
+    name:String;
+    description:String;
+    world:number;
+    page:number;
+	x:number;
+	y:number;
+}
---
+++ src/app/mock-animation.ts
@@ -0,0 +1,11 @@
+import { Animation } from "./Animation";
+import { WORLDS } from "./mock-worlds";
+ 
+export const ANIMATION: Animation[] = [
+    {id:1, name:'cat', description:'', world:WORLDS[0].id, page:4, x:1085, y:-164},
+    //{id:2, name:'boar', description:'', world:WORLDS[0].id, animation:ANIMATION[4], page:4},
+    //{id:3, name:'rabbit', description:'', world:WORLDS[1].id, animation:ANIMATION[4], page:4},
+    //{id:4, name:'squirrel', description:'', world:WORLDS[1].id, animation:ANIMATION[4], page:4},
+    //{id:5, name:'fox', description:'', world:WORLDS[2].id, animation:ANIMATION[4], page:4},
+    //{id:6, name:'dog', description:'', world:WORLDS[2].id, animation:ANIMATION[4], page:4}
+  ];
