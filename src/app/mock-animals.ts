b8fb3047ac9107d830f06370eaf72b9d27fe6761
João Afonso <johnlandon1999@gmail.com>
2021-06-08 00:40:39 +0100

    Video do cao pronto

--- src/app/mock-animals.ts
+++ src/app/mock-animals.ts
@@ -7,5 +7,5 @@ export const ANIMALS: Animal[] = [
     {id:3, name:'frog', description:'', imageUrl:'../assets/img/frog.png', catched:false, world:WORLDS[1].id, page:1, x:0.4855, y:0.3934, link:"https://en.wikipedia.org/wiki/Frog"},
     {id:4, name:'koala', description:'', imageUrl:'../assets/img/koala.png', catched:false, world:WORLDS[1].id, page:2, x:0.5914, y:0.5759, link:"https://en.wikipedia.org/wiki/Koala"},
     {id:5, name:'fox', description:'', imageUrl:'../assets/img/fox.png', catched:false, world:WORLDS[2].id, page:4, x:0.1, y:0.1, link:"https://en.wikipedia.org/wiki/Fox"},
-    {id:6, name:'dog', description:'', imageUrl:'../assets/img/dog.png', catched:false, world:WORLDS[2].id, page:4, x:0.1, y:0.1, link:"https://en.wikipedia.org/wiki/Dog"},
+    {id:6, name:'dog', description:'', imageUrl:'../assets/img/dog.png', catched:false, world:WORLDS[2].id, page:2, x:0.28, y:0.18, link:"https://en.wikipedia.org/wiki/Dog"},
   ];
